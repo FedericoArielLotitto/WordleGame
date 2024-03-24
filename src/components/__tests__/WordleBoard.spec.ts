65726d29cf1fc35546cc3fc913dc84431e2470dc
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-03-24 13:39:38 -0300

    Reduce repetition in tests

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -24,15 +24,13 @@ describe('WordleBoard', () => {
       expect(wrapper.text()).toContain(VICTORY_MESSAGE)
     })
   
-    describe.each([
-      {numberOfGuesses: 0, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: 1, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: 2, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: 3, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: 4, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: 5, shouldSeeDefeatMessage: false},
-      {numberOfGuesses: MAX_GUESSES_COUNT, shouldSeeDefeatMessage: true},
-    ])("A defeat message should appear if the player meakes incorrect guesses 6 times in a row", ({numberOfGuesses, shouldSeeDefeatMessage}) => {
+    describe.each(
+      Array.from({length: MAX_GUESSES_COUNT + 1},
+        (_, numberOfGuesses) => ({
+          numberOfGuesses,
+          shouldSeeDefeatMessage: numberOfGuesses === MAX_GUESSES_COUNT
+        }))
+    )("A defeat message should appear if the player meakes incorrect guesses 6 times in a row", ({numberOfGuesses, shouldSeeDefeatMessage}) => {
       test(`therefore for ${numberOfGuesses} guess(es), a defeat message should ${shouldSeeDefeatMessage ? "" : "not"} appear`, async () => {
         for (let i = 0; i < numberOfGuesses; i++) {
           await playerSubmitsGuess("WRONG")
