9e9811045a9f61affef3497408511ea23da0c62e
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-03-27 00:25:25 -0300

    Leaves only dist folder

--- src/settings.ts
+++
@@ -1,4 +0,0 @@
-export const VICTORY_MESSAGE = "You won!"
-export const WRONG_GUESS_MESSAGE = "Wrong guess :( Try again tomorrow with the new word!" 
-export const WORD_SIZE = 5
-export const MAX_GUESSES_COUNT = 6
