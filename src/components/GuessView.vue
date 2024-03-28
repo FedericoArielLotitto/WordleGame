311f117bb0c1c223bcfbc135d02e0b44cbb138d5
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-03-27 22:25:13 -0300

    Shows feedback of the letters of the word guessed

--- src/components/GuessView.vue
+++ src/components/GuessView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { WORD_SIZE } from '@/settings';
 
-const props = defineProps<{ guess: string, answer?: string, isWrongWord?: boolean }>()
+const props = defineProps<{ guess: string, answer?: string }>()
 
 function getFeedback(letterPosition: number): null | 'correct' | 'incorrect' | 'almost' {
   if (!props.answer) {
@@ -26,6 +26,7 @@ function getFeedback(letterPosition: number): null | 'correct' | 'incorrect' | '
     :data-letter-feedback="getFeedback(index)"
     :data-letter="letter"
     class="letter text-secondary"
+    :class="{ 'is-flipping': answer }"
     v-text="letter">
     </li>
   </ul>
@@ -40,20 +41,26 @@ function getFeedback(letterPosition: number): null | 'correct' | 'incorrect' | '
   }
 
   .letter {
+    --front-color: #ffffff;
+    --back-color: #327813;
     list-style: none;
-    background-color: #ffffff;
+    background-color: var(--front-color);
     display: inline-block;
     align-content: center;
     text-align: center;
     height: 50px;
     width: 50px;
-    border: solid 2px #327813;
+    border: solid 2px var(--back-color);
     border-radius: 15px;
     font-size: 1.8rem;
     margin: 4px;
   }
 
-  li:not([data-letter=" "]) {
+  li:not([data-letter=" "],
+    [data-letter-feedback="incorrect"], 
+    [data-letter-feedback="correct"], 
+    [data-letter-feedback="almost"] 
+  ) {
     animation: pop 1000ms;
   }
 
@@ -64,6 +71,39 @@ function getFeedback(letterPosition: number): null | 'correct' | 'incorrect' | '
 
     50% {
       transform: scale(1.4);
+    }
+  }
+
+  [data-letter-feedback="correct"] {
+    border-color: var(--primary-color);
+  }
+
+  [data-letter-feedback="almost"] {
+    border-color: hsl(40, 65%, 48%);
+  }
+
+  [data-letter-feedback="incorrect"] {
+    border-color: #265116;
+  }
+
+  .is-flipping {
+    animation: flip 300ms forwards;
+    animation-delay: 250ms;
+  }
+
+  @keyframes flip {
+    0% {
+      transform: rotateY(0);
+    }
+
+    50% {
+      transform: rotateY(-90deg);
+    }
+
+    100% {
+      trasnform: rotateY(0);
+      border-width: 4px;
+      /* border-style: outset; */
     }
   }
 </style>
