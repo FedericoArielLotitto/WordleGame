9ed51cf596d40a8d0763ee41d655879679c32d7b
FedericoArielLotitto <federicoariellotitto@gmail.com>
2024-03-31 18:22:45 -0300

    Change word based on the day

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import WordleBoard from './components/WordleBoard.vue'
+import englishWords from '@/englishWordsWith5Letters.json'
+
+const wordOrder = ref(0)
+const initialDate = ref(new Date('2024-03-31'))
+
+function getAgeOfGameInDays(date: Date) {
+  const duration = Math.floor(date.getTime() - initialDate.value.getTime())
+  const absoluteDifferenceInDays = Math.floor(duration / (1000 * 60 * 60 * 24))
+  return absoluteDifferenceInDays
+}
+
+function updateInitialDate(date: Date) {
+  console.log(englishWords.length)
+  if (!englishWords[getAgeOfGameInDays(date)]) {
+    initialDate.value = new Date(date.getFullYear(), date.getMonth(), date.getDate()) 
+  }
+}
+const wordOfTheDay = computed<string>({
+  get() {
+    const today = new Date()
+    updateInitialDate(today)
+    let todayWord = englishWords[getAgeOfGameInDays(today)]
+    console.log(todayWord)
+    return todayWord
+  },
+  set() {
+
+  }
+})
 </script>
 
 <template>
-  <WordleBoard word-of-the-day="TESTS" />
+  <WordleBoard :word-of-the-day="wordOfTheDay" />
 </template>
 
 <style>
